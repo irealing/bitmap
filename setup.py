e64d98530c71166883e58afc55ed9fb44998fdc8
Memory_Leak <irealing@163.com>
2019-11-03 07:30:41 +0000

    modify: add version info

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import Extension, setup
 
 setup(
     name="bitmap",
+    version="0.0.1",
+    author="Memory_Leak",
+    author_email="irealing@163.com",
     ext_modules=[
-        Extension("bitmap._bitmap", ["src/bitmap.c", "src/py_bitmap.c"],
-                  include_dirs=["include"])
+        Extension(
+            "bitmap._bitmap",
+            ["src/bitmap.c", "src/py_bitmap.c"], include_dirs=["include"]
+        )
     ]
 )
