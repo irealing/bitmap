ee422e8114bf997d6ec92ad4f3bc9bd569ed43c3
Memory_Leak <irealing@163.com>
2019-11-03 08:50:26 +0000

    fixed: fix Segmentation fault

--- src/py_bitmap.c
+++ src/py_bitmap.c
@@ -2,7 +2,7 @@
 
 static int Bitmap_init(PyBitmap *self, PyObject *args, PyObject *kw) {
   uint64_t items = 0, offset = 0;
-  if (!PyArg_ParseTuple(args, "KK", &items, offset)) {
+  if (!PyArg_ParseTuple(args, "KK", &items, &offset)) {
     return -1;
   }
   self->raw = newBitmap(items, offset);
@@ -16,13 +16,14 @@ static void Bitmap_dealloc(PyBitmap *self) {
 }
 
 static PyTypeObject BitmapType = {
-    PyVarObject_HEAD_INIT(NULL, 0).tp_name = "bitmap.Bitmap",
+    PyVarObject_HEAD_INIT(NULL, 0).tp_name = "Bitmap",
     .tp_doc = "Python Bitmap Extension",
     .tp_basicsize = sizeof(PyBitmap),
     .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_new = PyType_GenericNew,
     .tp_init = (initproc)Bitmap_init,
     .tp_dealloc = (destructor)Bitmap_dealloc,
+    .tp_methods = Bitmap_methods,
 };
 
 PyMODINIT_FUNC PyInit__bitmap(void) {
@@ -30,27 +31,29 @@ PyMODINIT_FUNC PyInit__bitmap(void) {
   if (PyType_Ready(&BitmapType) < 0) {
     return NULL;
   }
-  if ((m = PyModule_Create(&bitmapmodule)) == NULL) {
+  m = PyModule_Create(&bitmapmodule);
+  if (m == NULL) {
     return NULL;
   }
   Py_INCREF(&BitmapType);
   if (PyModule_AddObject(m, "Bitmap", (PyObject *)&BitmapType) < 0) {
     Py_DECREF(&BitmapType);
     Py_DECREF(m);
+    return NULL;
   }
   return m;
 }
 
 static PyObject *Bitmap_add(PyBitmap *self, PyObject *args) {
   uint64_t val = 0;
-  if (!PyArg_Parse(args, "K", &val)) {
+  if (!PyArg_ParseTuple(args, "K", &val)) {
     return Py_None;
   }
   return PyLong_FromUnsignedLongLong(bitmapAdd(self->raw, val));
 }
 static PyObject *Bitmap_has(PyBitmap *self, PyObject *arg) {
   uint64_t val = 0;
-  if (!PyArg_Parse(arg, "K", &val)) {
+  if (!PyArg_ParseTuple(arg, "K", &val)) {
     return Py_None;
   }
   return PyLong_FromUnsignedLongLong(bitmapHas(self->raw, val));
